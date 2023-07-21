b6fa62b3c22c3ac185278539d9c38689fa9760c6
EsraTosun <114695418+EsraTosun@users.noreply.github.com>
2023-07-21 20:11:24 +0300

    Add files via upload

---
+++ Chechbox_Slider_Radio_ve_Switch_Widget/android/app/src/main/kotlin/com/example/dort_chechbox_slider_radio_ve_switch_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dort_chechbox_slider_radio_ve_switch_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Chechbox_Slider_Radio_ve_Switch_Widget/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\esrat\Flutter(Emre Altunbilek)\alti_kullanicidan_veri_alabilcegimiz_input_widgetlar\dort_Chechbox_Slider_Radio_ve_Switch_Widget"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
